4eeca89e26b3121aae7ca999d6dbab614ae7f76c
ALEXSANDRO PAULINO DANTAS <alexsandro.dantas@SEFAZ.PE.GOV.BR>
2023-01-06 14:05:55 -0300

    padrao

---
+++ server/src/main/java/com/illucit/ejbremote/server/ExampleServiceImpl.java
@@ -0,0 +1,28 @@
+package com.illucit.ejbremote.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ejb.Stateless;
+
+/**
+ * Implementation of {@link ExampleService}.
+ * 
+ * @author Christian Simon
+ *
+ */
+@Stateless
+public class ExampleServiceImpl implements ExampleService {
+
+	@Override
+	public String greet(String name) {
+		System.out.println("Aqui");
+		return "Hello " + name + "!";
+	}
+
+	@Override
+	public Map<Object, Object> getSystemProperties() {
+		return new HashMap<>(System.getProperties());
+	}
+
+}
